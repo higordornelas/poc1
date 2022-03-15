856ad432715819f81300617e7135137859a17d40
higordornelas <higorsdornelas@gmail.com>
2022-03-15 11:19:59 -0300

    adicionado o Patch em ambos Controllers

--- src/main/java/com/higor/poc1/api/controller/CustomerController.java
+++ src/main/java/com/higor/poc1/api/controller/CustomerController.java
@@ -1,5 +1,6 @@
 package com.higor.poc1.api.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.higor.poc1.domain.model.Customer;
 import com.higor.poc1.domain.repository.CustomerRepository;
 import org.springframework.beans.BeanUtils;
@@ -7,10 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Field;
 import java.net.URI;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/customers")
@@ -59,6 +63,33 @@ public class CustomerController {
         } catch (NullPointerException e) {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @PatchMapping("/{customerId}")
+    public ResponseEntity<?> patchCustomer(@PathVariable Long customerId, @RequestBody Map<String, Object> fields) {
+        Customer thisCustomer = customerRepository.find(customerId);
+
+        try {
+            merge(fields, thisCustomer);
+
+            return ResponseEntity.ok(thisCustomer);
+        } catch(Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    private void merge(Map<String, Object> sourceFields, Customer customerToPatch) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Customer thisCustomer = objectMapper.convertValue(sourceFields, Customer.class);
+
+        sourceFields.forEach((propertyName, propertyValue) -> {
+            Field field = ReflectionUtils.findField(Customer.class, propertyName);
+            field.setAccessible(true);
+
+            Object newValue = ReflectionUtils.getField(field, thisCustomer);
+
+            ReflectionUtils.setField(field, customerToPatch, newValue);
+        });
     }
 
     @DeleteMapping("/{customerId}")
